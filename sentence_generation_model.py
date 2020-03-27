8e016190b9a09c368a407f537b96f992324a7914
suleiman99hesham <suleimanhesham99@gmail.com>
2020-03-27 08:28:09 +0200

    adding new option for predicting

--- sentence_generation_model.py
+++ sentence_generation_model.py
@@ -12,7 +12,9 @@ data = file.read()
 file.close()
 
 # split data in single words
-data_tokens = nltk.word_tokenize(data)
+data=data.split()
+data_tokens = [w.strip(" ") for w in data]
+
 
 # creating trigrams from tokens
 data_trigram=[]
@@ -37,15 +39,46 @@ for w1_w2 in model:
 sentence=input("write your words: ")  #for dynamic input
 print("loading...!")
 
-# predicting the ten words after the input sentence
-for counter in range(10):
+
+# option1
+# predicting the ten words after getting some words (more than two) / predicting complete sentence
+
+# for counter in range(10):
+#     sentence_temp = nltk.word_tokenize(sentence)
+#     sentence_temp = tuple(sentence_temp[-2:])
+#     for w1_w2 in model:
+#         # print(w1_w2)
+#         if sentence_temp == w1_w2:
+#             max_prop_word = max(model[w1_w2].items(), key=lambda a: a[1])
+#             sentence += " " + max_prop_word[0]
+#
+# # the final result
+# print("predicted sentence: ",sentence)
+
+
+#Option2
+#showing a list of predicted words and choose between them
+
+while True:
     sentence_temp = nltk.word_tokenize(sentence)
     sentence_temp = tuple(sentence_temp[-2:])
     for w1_w2 in model:
-        # print(w1_w2)
         if sentence_temp == w1_w2:
-            max_prop_word = max(model[w1_w2].items(), key=lambda a: a[1])
-            sentence += " " + max_prop_word[0]
-
-# the final result
-print("predicted sentence: ",sentence)
+            print(sentence,"...")
+            i=1
+            predictions=sorted(model[w1_w2].items(),key=lambda t:(-t[1],t[0]))
+            for predicted_word in predictions:
+                if i==7:break
+                print(i,"-",predicted_word[0])
+                i+=1
+            choosed_word=int(input(">>"))
+            sentence+=" "+predictions[choosed_word-1][0]
+    option = input("continue? Y or N").lower()
+    while option!="y" and option!="n":
+        print("Oops, wrong answer!")
+        option = input("continue? Y or N").lower()
+    if option == "y":
+        sentence +=" "+input(sentence+" ")
+        print("loading...!")
+    else :
+        break
